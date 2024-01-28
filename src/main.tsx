869851e49c8d25c085fb7bf7a86b2b452c6d198f
Geuni <dlrmsgnl620@naver.com>
2024-01-28 23:36:29 +0900

    Feat: setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,23 @@ import { Toaster } from 'sonner';
 import { App } from 'src/App';
 import { QueryProvider } from 'src/components/common/QueryProvider';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryProvider>
-      <Toaster />
-      <App />
-    </QueryProvider>
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('src/mocks/browser');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <QueryProvider>
+        <Toaster />
+        <App />
+      </QueryProvider>
+    </React.StrictMode>,
+  );
+});
