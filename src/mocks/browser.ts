549113da7ece06c996a6d66a7222eaa6bb847e60
Geuni <dlrmsgnl620@naver.com>
2024-04-19 01:19:52 +0900

    feat: add setupTest msw for vitest

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,4 @@
-import { setupWorker } from 'msw/browser';
+import { setupServer } from 'msw/node';
 import { handlers } from 'src/mocks/handlers';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupServer(...handlers);
