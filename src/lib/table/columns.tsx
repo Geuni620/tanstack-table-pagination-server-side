1ad27cc1fde9a15df523e718dcdbebf229b687fa
Geuni <dlrmsgnl620@naver.com>
2024-05-15 23:39:18 +0900

    feat: add columns

---
+++ src/lib/table/columns.tsx
@@ -0,0 +1,43 @@
+import { createColumnHelper } from '@tanstack/react-table';
+
+import { Checkbox } from '@/components/ui/checkbox';
+import { type TaskProps } from '@/hooks/useTaskGetQuery';
+
+type ExtendedTaskProps = TaskProps & {
+  select: string;
+};
+
+const columnHelper = createColumnHelper<ExtendedTaskProps>();
+export const columns = [
+  columnHelper.accessor('select', {
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  }),
+  columnHelper.accessor('task', {
+    header: 'Task',
+    cell: (props) => <p>{props.getValue()}</p>,
+  }),
+  columnHelper.accessor('statusName', {
+    header: 'Status',
+    cell: (props) => <p>{props.getValue()}</p>,
+  }),
+  columnHelper.accessor('date', {
+    header: 'Due',
+    cell: (props) => <p>{props.getValue()}</p>,
+  }),
+];
